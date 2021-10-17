cbd54f542272dfea5c789dc0712e32f1630c9e81
Vcvc <vcvckw@gmail.com>
2021-10-17 21:23:05 +0800

    ui2

--- ui2/View/View/CircleImage.swift
+++ ui2/View/View/CircleImage.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            .shadow(radius: 7)
     }
 }
 
--- ui2/View/View/ContentView.swift
+++ ui2/View/View/ContentView.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                HStack {
+                    Text("Joshua Tree Nation Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
             .padding()
-    }
+            Spacer()
+        }
+            
+     }
 }
 
 struct ContentView_Previews: PreviewProvider {
